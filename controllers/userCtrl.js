1b049599b61c65112f96f85b1e45ea4a6a0058ea
Weisheng Wu <weishenggwuu@gmail.com>
2019-03-03 01:45:26 -0800

    webscrape html from url request

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,17 +1,19 @@
 const calendarCtrl = require('./calendarCtrl');
+const request = require('request');
+
 
 module.exports = {
     connectCalendar: async (req, res) => {
-        try{
+        try {
             const { access_token, refresh_token } = req.body;
             // await User.save(access_token, refresh_token);
-    
+
             const calendarData = await calendarCtrl.getCalendarFromGoogle(access_token, refresh_token);
 
             res.status(201).send({
                 calendarData
             });
-        }catch(err) {
+        } catch (err) {
             res.status(404).send({
                 err
             });
@@ -21,17 +23,26 @@ module.exports = {
         console.log('test');
     },
     getDailyEvents: async (req, res) => {
-        try{
+        try {
             // Get free times from database----
             // const bitwise = await User.get(bitwiseCalendar)
-            
+
             // const events = Scrape EventBrite for events (Cheerio)
+            const request = require('request');
+            const cheerio = require('cheerio');
+
+            request('https://www.eventbrite.com/', function (err, resp, html) {
+                if (!err) {
+                    const $ = cheerio.load(html);
+                    console.log(html);
+                }
+            });
             // const releventEvents = events.filter(event => //does event overlap with schedule ? true : false);
 
             res.status(201).send({
-                releventEvents
+                releventEventsß
             });
-        }catch(err) {
+        } catch (err) {
             res.status(404).send({
                 err
             });
